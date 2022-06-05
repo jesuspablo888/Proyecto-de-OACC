1a7b7501675430fa28694c4d5828fbd6084f28bc
Jesús Pablo <48724931+jesuspablo888@users.noreply.github.com>
2022-06-04 19:05:45 -0500

    Add files via upload

--- BaseDeDatos/src/fes/aragon/mysql/Conexion.java
+++ BaseDeDatos/src/fes/aragon/mysql/Conexion.java
@@ -5,7 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import fes.aragon.modelo.Clientes;
+import fes.aragon.modelo.Facturas;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -98,5 +100,95 @@ public class Conexion {
 		conexion.close();
 	}
 
+	//FACTURAS
+	
+	public ObservableList<Facturas> buscarFacturas() throws SQLException {
+		ObservableList<Facturas> lista = FXCollections.observableArrayList();
+		String query = "{call buscarFacturas()}";
+		CallableStatement solicitud = conexion.prepareCall(query);
+		ResultSet datos = solicitud.executeQuery(query);
+		if (!datos.next()) {
+			System.out.println("no hay datos");
+		} else {
+			do {
+				Facturas fac = new Facturas();
+				fac.setIdF(datos.getInt(1));
+				fac.setReferencia(datos.getString(2));
+				fac.setFecha(datos.getDate(3));
+				Clientes cl = new Clientes();
+				cl.setId(datos.getInt(4));
+				cl.setNombre(datos.getString(5));
+				cl.setApellidoPaterno(datos.getString(6));
+				fac.setCliente(cl);
+				lista.add(fac);
+			} while (datos.next());
+		}
+		datos.close();
+		solicitud.close();
+		conexion.close();
+		return lista;
+	}
+	
+	public ObservableList<Facturas> buscarFact(String patron) throws SQLException {
+		ObservableList<Facturas> lista = FXCollections.observableArrayList();
+		String queryBP = "{call buscarFact(?)}";
+		CallableStatement solicitud = conexion.prepareCall(queryBP);
+		solicitud.setString(1, patron);
+		ResultSet datos = solicitud.executeQuery();
+		if (!datos.next()) {
+			System.out.println("no hay datos");
+		} else {
+			do {
+				Facturas fac = new Facturas();
+				fac.setIdF(datos.getInt(1));
+				fac.setReferencia(datos.getString(2));
+				fac.setFecha(datos.getDate(3));
+				Clientes cl = new Clientes();
+				cl.setId(datos.getInt(4));
+				cl.setNombre(datos.getString(5));
+				cl.setApellidoPaterno(datos.getString(6));
+				fac.setCliente(cl);
+				lista.add(fac);
+			} while (datos.next());
+		}
+		datos.close();
+		solicitud.close();
+		conexion.close();
+		return lista;
+	}
+	
+	public void almacenarFactura(Facturas fac) throws SQLException {
+		String query = "{call insertarFacturas(?,?,?)}";
+		CallableStatement solicitud = conexion.prepareCall(query);
+		solicitud.setInt(1, fac.getIdCl());
+		solicitud.setString(2, fac.getReferencia());
+		solicitud.setDate(3, fac.getFecha());
+		solicitud.execute();
+		solicitud.close();
+		conexion.close();
+	}
+
+	public void eliminarFactura(Integer id) throws SQLException {
+		String query = "{call eliminarFactura(?)}";
+		CallableStatement solicitud = conexion.prepareCall(query);
+		solicitud.setInt(1, id);
+		solicitud.execute();
+		solicitud.close();
+		conexion.close();
+
+	}
+
+	public void modificarFactura(Facturas fac) throws SQLException {
+		String query = "{call modificarFacturas(?,?,?,?)}";
+		CallableStatement solicitud = conexion.prepareCall(query);
+		solicitud.setInt(1, fac.getIdF());
+		System.out.println("id cliente: "+fac.getIdCl());
+		solicitud.setInt(2, fac.getIdCl());
+		solicitud.setString(3, fac.getReferencia());
+		solicitud.setDate(4, fac.getFecha());
+		solicitud.execute();
+		solicitud.close();
+		conexion.close();
+	}
 	
 }
